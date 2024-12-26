c146ae6b39a91c0c7de01e455ed3ec94a4e3cab1
heynar-max <heynar76@hotmail.com>
2024-12-26 18:15:25 -0500

    Creando rutas de la aplicación

---
+++ src/app/(music)/admin/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function adminMusicPage() {
+    return (
+        <div>
+        <h1>Admin Page</h1>
+        </div>
+    );
+}
---
+++ src/app/(music)/category/[id]/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default async function categoryIdPage() {
+    
+    return (
+        <div>
+            <h1>category Page</h1>
+        </div>
+    );
+}
