110b72ab3a2a30800ca86e346f475391fb60d008
heynar-max <heynar76@hotmail.com>
2025-05-10 19:24:36 -0500

    Formulario para crear usuarios y autenticar

--- src/app/auth/new_account/page.tsx
+++ src/app/auth/new_account/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import { RegisterForm } from "./ui/Register";
+
 
 
 export default function New_Account() {
@@ -7,51 +8,7 @@ export default function New_Account() {
             <div className="login-card">
                 <h1 className={ ` text-4xl mb-5` }>Nueva Cuenta</h1>
 
-                <div className="flex flex-col">
-
-                    <label htmlFor="fullName">Nombre Completo</label>
-                    <input
-                    id="fullName"
-                    className="login-input bg-gray-200"
-                    type="text"
-                    autoComplete="name" />
-
-                    <label htmlFor="email">Correo electrónico</label>
-                    <input
-                    id="email"
-                    className="login-input bg-gray-200"
-                    type="email"
-                    autoComplete="email" />
-
-
-                    <label htmlFor="password">Contraseña</label>
-                    <input
-                    id="password"
-                    className="login-input bg-gray-200"
-                    type="email"
-                    autoComplete="new-password"/>
-
-                    <button
-                    
-                    className="btn-butonprimary">
-                    Crear cuenta
-                    </button>
-
-
-                    {/* divisor l ine */ }
-                    <div className="flex items-center my-5">
-                    <div className="flex-1 border-t border-gray-500"></div>
-                    <div className="px-2 text-gray-800">O</div>
-                    <div className="flex-1 border-t border-gray-500"></div>
-                    </div>
-
-                    <Link
-                    href="/auth/login" 
-                    className="btn-buton hover:bg-sky-200 text-black py-2 px-4 rounded transition-all  text-center">
-                    Ingresar
-                    </Link>
-
-                </div>
+                <RegisterForm/>
             </div>
     </div>
     
